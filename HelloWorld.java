b4ea70111dcab50be2f6624895ca929f0a4cd393
Daniel Estrada <daniel.nicholas.estrada@gmail.com>
2023-12-06 11:16:45 -0600

    java syntax data types var exercise

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,37 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
         System.out.print("World!");
+        int myFavoriteNumber = 9;
+        System.out.println(myFavoriteNumber);
+        String myString = "stuff";
+        System.out.println(myString);
+        double myNumber = 3.14E00;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++); x is 5, it is incremented after it is run
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x); // x is incremented then prints 6
+//        System.out.println(x);
+//        String class = "class";   not a statement
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;    having issues converting object to int
+//        int three = (int) "three";    String cannot be converted to int
+//        int x = 4;
+//        x += 5; //x = x + 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x; //y = y * x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+//        x /= y; x = x / y;
+//        System.out.println(x);
+        y-= x;  //y = y - x;
+        System.out.println(y);
+//        byte num = 1024;  lossy conversion
+        System.out.println(Integer.MAX_VALUE); // 2147483647, 4 bytes allocated
     }
 }
